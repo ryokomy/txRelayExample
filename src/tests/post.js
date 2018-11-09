0730179df5b321a3678213ee90bdc20cf44d01bf
Ryo Komiyama <komikomi.mikomiko@gmail.com>
2018-11-09 23:20:21 +0900

    Update post.js to implement sign tx, but still has bug

--- src/tests/post.js
+++ src/tests/post.js
@@ -27,18 +27,85 @@ async function post(api, form) {
     }
 }
 
+// web3 *********************************************************************************
+const Web3 = require("web3");
+const MyContractInfo = require('../../build/contracts/MyContract.json')
+
+// provider 
+const provider = new Web3.providers.HttpProvider("http://127.0.0.1:7545");
+
+// web3
+const web3 = new Web3(provider);
+
+// account
+const ownerPrivKey = '1ecf00c187e29992c9e74d2ac2a1c385706835da64963bbbbd4128b8da856d0e'
+const ownerAccount = web3.eth.accounts.privateKeyToAccount('0x' + ownerPrivKey)
+web3.eth.accounts.wallet.add(ownerAccount); // web3.eth.accounts.wallet[0] == ownerAccount
+
+// abi
+let mycontract_abi = MyContractInfo.abi
+
+// address
+let mycontract_address = MyContractInfo.networks["5777"].address
+
+// contract
+const MyContract = new web3.eth.Contract(mycontract_abi, mycontract_address)
+// **************************************************************************************
+
+
 async function postTxRelay() {
     let api = '/txRelay'
-    let form = {
-        tx: 'test string'
+
+    console.log('f: setAddress')
+    let addr = '0x452E251e92a10D81d135c056eE7ED3E4A47cD65B';
+    let txData = MyContract.methods.setAddress(addr).encodeABI()
+    // let txData = MyContract.methods.incrementX().encodeABI()
+    console.log(txData)
+
+    let tx = {
+        nonce: await web3.eth.getTransactionCount(ownerAccount.address),
+        from: ownerAccount.address,
+        gasPrice: '10000000000',
+        gas:6000000,
+        data: txData
+    }
+    let signedTx = await web3.eth.accounts.signTransaction(tx, ownerAccount.privateKey)
+
+    let receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction)
+    console.log(receipt)
+
+    {
+        console.log('f: x')
+        let x = await MyContract.methods.x().call()
+        .catch(err => {throw err})
+        console.log(`x = ${x}`)
+        console.log()
+    }
+    {
+        console.log('f: addr')
+        let addr = await MyContract.methods.addr().call()
+        .catch(err => {throw err})
+        console.log(`addr = ${addr}`)
+        console.log()
+    }
+    {
+        console.log('f: sender')
+        let sender = await MyContract.methods.sender().call()
+        .catch(err => {throw err})
+        console.log(`sender = ${sender}`)
+        console.log()
     }
 
-    let result = await post(api, form)
-    .catch(err => {
-        console.error(err)
-    })
+    // let form = {
+    //     tx: 'test string'
+    // }
 
-    return result
+    // let result = await post(api, form)
+    // .catch(err => {
+    //     console.error(err)
+    // })
+
+    // return result
 }
 
 async function test() {
